1933b38e400d25b6d272aea9b047f3ae86c1bccb
Kristina Tong <kristina.tong@gmail.com>
2018-07-09 20:41:38 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,13 @@
+function theBeatlesPlay(musicians,instruments){
+  array = []
+  for (var i = 0; i<musicians.length; i++){
+    array.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return arrray
+}
+
+function johnLennonFacts(facts){
+  while(i < facts.length){
+    facts[i].push('!!!')
+  }
+}
